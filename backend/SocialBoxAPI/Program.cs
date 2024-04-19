cbac043d5a5058c5a7adf26d64693d82a583cc23
Zoldier777 <Zzer0ph@gmail.com>
2024-04-19 11:14:39 +0200

    setup: test

--- backend/SocialBoxAPI/Program.cs
+++ backend/SocialBoxAPI/Program.cs
@@ -10,8 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
